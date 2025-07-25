1d956def4a7bb6639b0568c12c12f1a6bc86c64f
Julian Hans <julian.hans@student.uni-luebeck.de>
2025-07-25 11:16:21 +0200

    nvm

--- scripts/main.sh
+++ scripts/main.sh
@@ -7,10 +7,10 @@ FUZZER_NAME="TestFuzzer"
 TESTCASE_FILE="out/static_corpus/test.txt"
 
 # Build image
-build_image.sh "$PROJECT_NAME"
+./scripts/build_image.sh "$PROJECT_NAME"
 
 # Build fuzzers
-build_fuzzers.sh "$PROJECT_NAME" "$SANITIZER_TYPE" "$FUZZER_NAME"
+./scripts/build_fuzzers.sh "$PROJECT_NAME" "$SANITIZER_TYPE" "$FUZZER_NAME"
 
 # Run fuzzer
-run_fuzz.sh "$PROJECT_NAME" "$SANITIZER_TYPE" "$FUZZER_NAME" "$TESTCASE_FILE"
+./scripts/run_fuzz.sh "$PROJECT_NAME" "$SANITIZER_TYPE" "$FUZZER_NAME" "$TESTCASE_FILE"
